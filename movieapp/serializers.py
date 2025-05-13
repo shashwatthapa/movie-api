547395d80f5b9eafb9a001b5359b84ecbbf0d397
shashwatthapa <thapashashwat@gmail.com>
2025-05-13 13:02:25 +0545

    User registration serializer created

---
+++ movieapp/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
+    class Meta:
+        model = User 
+        fields= ['username','email','password','password2']
+    
+    def validate(self,attrs):
+        if attrs['password']!=attrs['password2']:
+            raise serializers.ValidationError({"password":"Password fields doesnt match"})
+        
+    def create(self,validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user 
--- movieproject/settings.py
+++ movieproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'movieapp',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
